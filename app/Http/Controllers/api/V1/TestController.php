896c2c1ad53ba427638d83cf107f4797bbe4a4f1
shofiudinmuh <shofiudinmuh@gmail.com>
2024-09-30 12:14:35 +0700

    validate & store a data on database

--- app/Http/Controllers/api/V1/TestController.php
+++ app/Http/Controllers/api/V1/TestController.php
@@ -22,17 +22,20 @@ class TestController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+
+    // public function create()
+    // {
+    //     //
+    // }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTestRequest $request)
     {
-        //
+        $test = Test::create($request->validated());
+
+        return TestResource::make($test);
     }
 
     /**
